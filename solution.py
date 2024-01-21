da918b20d89027d0197cc02f32b6283eb691879d
Kooreis <kooreis.official@gmail.com>
2024-01-21 14:16:41 -0600

    This part of the script defines a function to correct the spelling of a word by finding the candidate word with the highest probability.

--- solution.py
+++ solution.py
@@ -1,9 +1,2 @@
-import re
-from collections import Counter
-
-def words(text): return re.findall(r'\w+', text.lower())
-
-WORDS = Counter(words(open('big.txt').read()))
-
-def P(word, N=sum(WORDS.values())): 
-    return WORDS[word] / N
+def correction(word): 
+    return max(candidates(word), key=P)
